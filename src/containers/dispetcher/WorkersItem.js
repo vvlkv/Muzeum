5d91ad955712f79b3440905dd6777ef75215ac76
vvlkv <vvlkv@icloud.com>
2018-11-10 07:59:43 +0300

    new screens

--- src/containers/dispetcher/WorkersItem.js
+++ src/containers/dispetcher/WorkersItem.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import * as UI from '@vkontakte/vkui';
 import {connect} from 'react-redux';
+import autoBind from 'react-autobind';
 import {push} from 'react-router-redux';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import Icon24Search from '@vkontakte/icons/dist/24/search';
@@ -9,8 +10,13 @@ class WorkersItem extends Component {
 
   constructor(props) {
     super(props);
+    autoBind(this);
     this.state = {
-			activePanel: 'workers'
+      activeView: "main",
+			activePanel: "workers",
+      workerID: -1,
+      post: -1,
+      password: ""
 		}
   }
 
@@ -26,33 +32,53 @@ class WorkersItem extends Component {
       </UI.View>
     );
   }
+
+  changeID(e) {
+    this.setState({workerID: e.target.value});
+  }
+
+  changePost(e) {
+    this.setState({post: e.target.value});
+  }
+
+  changePassword(e) {
+    this.setState({password: e.target.value});
+  }
+
   render() {
-    // if (!this.props.jobTypes) return this.preRender();
     return (
-      <UI.View id="workers" activePanel="workers">
-        <UI.Panel id='workers'>
-          <UI.PanelHeader
-            left={<UI.HeaderButton><Icon24Back/></UI.HeaderButton>}/>
-        </UI.Panel>
-      </UI.View>
+      <UI.Root activeView={this.state.activeView}>
+        <UI.View id="main" activePanel={this.state.activePanel}>
+          <UI.Panel id='workers'>
+            <UI.PanelHeader
+              left={<UI.HeaderButton onClick={() => this.setState({ activePanel: "addworker"})}>Добавить</UI.HeaderButton>}>
+              Сотрудники
+            </UI.PanelHeader>
+          </UI.Panel>
+          <UI.Panel id="addworker">
+            <UI.PanelHeader>Добавить</UI.PanelHeader>
+            <UI.FormLayout>
+              <UI.Input
+                type="ID"
+                top="ID сотрудника"
+                bottom="Введите ID ВКонтакте сотрудника, которого необходимо зарегистрировать"
+                onChange={this.changeID}/>
+              <UI.Select placeholder="Должность" onChange={this.changePost}>
+              </UI.Select>
+              <UI.Input
+                type="password"
+                top="Пароль"
+                onChange={this.changeID}/>
+            </UI.FormLayout>
+            <UI.Button size="xl" level="secondary" alignment="bottom">Добавить сотрудника</UI.Button>
+          </UI.Panel>
+        </UI.View>
+      </UI.Root>
     );
-  }
-
-  showRequest() {
-    this.setState({activePanel:'request'})
-  }
-
-  showWorkerEpic() {
-    this.setState({activePanel:'workerepic'})
-  }
-
-  showDispetcherEpic() {
-    this.setState({activePanel:'dispetcherepic'})
   }
 }
 
 function mapStateToProps(state) {
-  console.log("mapStateToProps");
   return {
   };
 }
