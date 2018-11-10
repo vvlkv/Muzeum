6f5c8eee2c4b003822be7a09c64eef9f5f7b380b
vvlkv <vvlkv@icloud.com>
2018-11-10 17:27:08 +0300

    Updates

---
+++ precache-manifest.1c893949162c518878dbf3041921e832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7543403eb4c5a9809cc",
+    "url": "/static/js/main.a7543403.chunk.js"
+  },
+  {
+    "revision": "a8086fa9529a2ff01062",
+    "url": "/static/css/1.6935373f.chunk.css"
+  },
+  {
+    "revision": "a8086fa9529a2ff01062",
+    "url": "/static/js/1.a8086fa9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cedf682e0fcdb1e9f0be31beb40ac727",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Muzeum/precache-manifest.216979d99073c4274385bb8604872c5a.js"
+  "/precache-manifest.1c893949162c518878dbf3041921e832.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Muzeum/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
