d976bcee18740ca0faad90970d49986e9bb50db4
vvlkv <vvlkv@icloud.com>
2018-11-10 15:22:24 +0300

    Updates

---
+++ precache-manifest.4bcedd5bad63b690b60e008b2a4a9075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91f750f8be30f70025f0",
+    "url": "/Muzeum/static/js/main.91f750f8.chunk.js"
+  },
+  {
+    "revision": "c13d618caca203931b5c",
+    "url": "/Muzeum/static/css/1.4114ac4d.chunk.css"
+  },
+  {
+    "revision": "c13d618caca203931b5c",
+    "url": "/Muzeum/static/js/1.c13d618c.chunk.js"
+  },
+  {
+    "revision": "e6a34f2d61c2b3f88781",
+    "url": "/Muzeum/static/js/runtime~main.e6a34f2d.js"
+  },
+  {
+    "revision": "88585c375bde7742bceb22ff3f4bc575",
+    "url": "/Muzeum/index.html"
+  }
+];
