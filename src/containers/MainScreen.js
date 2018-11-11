13008d79aa30baef932f3076419a2628d96f122e
vvlkv <vvlkv@icloud.com>
2018-11-11 09:37:17 +0300

    fixes

--- src/containers/MainScreen.js
+++ src/containers/MainScreen.js
@@ -43,7 +43,7 @@ class MainScreen extends Component {
           <Panel id='mainscreen'>
             <PanelHeader ></PanelHeader>
             <Button size="xl" level="2" onClick={this.showRequest.bind(this)}>
-              Посетитель15
+              Посетитель
             </Button>
             <Button size="xl" level="2" onClick={this.showWorkerEpic.bind(this)}>
               Работник
