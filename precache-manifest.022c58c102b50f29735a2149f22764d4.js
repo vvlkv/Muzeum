e55c689afc927700fbc666341c433c487174df11
Alexey <32864666+EvdokimovAlexey@users.noreply.github.com>
2018-11-11 10:35:03 +0300

    Updates

---
+++ precache-manifest.022c58c102b50f29735a2149f22764d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "152dfc697caece112291",
+    "url": "/Muzeum/static/js/main.152dfc69.chunk.js"
+  },
+  {
+    "revision": "9fee3af4946170e8be90",
+    "url": "/Muzeum/static/css/1.37b78080.chunk.css"
+  },
+  {
+    "revision": "9fee3af4946170e8be90",
+    "url": "/Muzeum/static/js/1.9fee3af4.chunk.js"
+  },
+  {
+    "revision": "e6a34f2d61c2b3f88781",
+    "url": "/Muzeum/static/js/runtime~main.e6a34f2d.js"
+  },
+  {
+    "revision": "6f538775746d7918f4c6d4aaf403bd13",
+    "url": "/Muzeum/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Muzeum/precache-manifest.9e14fc747a422471856a1cebfeaee662.js"
+  "/Muzeum/precache-manifest.022c58c102b50f29735a2149f22764d4.js"
 );
 
 workbox.clientsClaim();
